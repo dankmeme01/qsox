4eea05efce460c78f95b1d8abbad2b8f2f9a51e7
dankmeme01 <42031238+dankmeme01@users.noreply.github.com>
2025-06-26 22:45:38 +0200

    map unix errors to Error enum

--- src/unix/Error.cpp
+++ src/unix/Error.cpp
@@ -4,8 +4,47 @@
 namespace qsox {
 
 Error Error::lastOsError(bool) {
-    // TODO: map into other types than Other
-    return fromOs(errno);
+    auto code = errno;
+
+    switch (code) {
+        case ETIMEDOUT:
+            return Code::TimedOut;
+        case ECONNREFUSED:
+            return Code::ConnectionRefused;
+        case EBADF:
+        case EINVAL:
+            return Code::InvalidArgument;
+        case ENOTSOCK:
+            return Code::NotASocket;
+        case EACCES:
+            return Code::AccessDenied;
+        case EFAULT:
+            return Code::InvalidPointerAddress;
+
+#if EAGAIN != EWOULDBLOCK // EAGAIN and EWOULDBLOCK are the same on linux
+        case EAGAIN:
+#endif
+        case EWOULDBLOCK:
+            return Code::WouldBlock;
+        case EINPROGRESS:
+        case EALREADY:
+            return Code::InProgress;
+        case EMSGSIZE:
+            return Code::MessageTooLong;
+        case EADDRINUSE:
+            return Code::AddressInUse;
+        case ENETDOWN:
+            return Code::NetworkDown;
+        case ECONNABORTED:
+            return Code::ConnectionAborted;
+        case ECONNRESET:
+            return Code::ConnectionReset;
+        case EISCONN:
+            return Code::IsConnected;
+        case ENOTCONN:
+            return Code::NotConnected;
+        default: return fromOs(code);
+    }
 }
 
 std::string Error::osMessage(int osCode) {
